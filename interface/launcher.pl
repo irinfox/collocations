162e4b849adb994d944194e5f51824a0fdf12fb1
krylova93@gmail.com <krylova93@gmail.com@c4b3c64d-08b0-b83d-dc27-9dd952dc0450>
2013-03-20 12:22:33 +0000

    update

--- interface/launcher.pl
+++ interface/launcher.pl
@@ -3,8 +3,12 @@ use utf8;
 use DBI;
 use Encode;
 use strict;
+use Cwd 'abs_path';
+use File::Basename;
 
 my $tmpID; #for ID
+my $dir = dirname(abs_path($0))."/";
+print $dir;
 
 #Database connect
 my $dbh = DBI->connect('DBI:mysql:collocs:localhost', 'colloc', 'H4eSaa1L') or die $DBI::errstr;
@@ -26,7 +30,7 @@ while (my $val = $str->fetchrow_hashref){
 	#print $tmpID;
 	my $launch = $val->{'launch'};
 	my $res_path = $val->{'path'};
-	my $upd = $dbh->prepare("UPDATE journal SET mode=1 WHERE numb=?");
+        my $upd = $dbh->prepare("UPDATE journal SET mode=1 WHERE numb=?");
 	$upd->execute($tmpID) or die $dbh->errstr;
 }
 	
@@ -37,8 +41,17 @@ while (my $val1 = $str1->fetchrow_hashref){
       
 	my $launch1 = $val1->{'launch'};
 	my $res_path1 = $val1->{'path'};
-	$tmpID = $val1->{'numb'};
-      my $status =  system("$launch1 > $res_path1");
+        $launch1 =~ s/(\.\.)/$dir$1/g;
+       #не печатает эту строчку 
+        print "launch: ".$launch1."\n";  
+	
+        #my $add = "/var/www/html/colloc/inetrface/"; #for correct work of crontab
+	my $abs_path = $dir.$res_path1;
+        
+        print "abs: ".$abs_path."\n";
+	
+        $tmpID = $val1->{'numb'};
+      my $status =  system("$launch1 > $abs_path");
 
 	if ($status){
 	  if ($?==-1){
@@ -53,7 +66,7 @@ while (my $val1 = $str1->fetchrow_hashref){
 	   }
 	}
 	#нужно проверить результат работы программы и записать, что скрипт отработал => можно забирать результат	
-	if (-e $res_path1){
+	if (-e $abs_path){
             my $upd2 = $dbh->prepare("UPDATE journal SET mode=2 WHERE numb=?");
             $upd2->execute($tmpID) or die $dbh->errstr;
 	}
